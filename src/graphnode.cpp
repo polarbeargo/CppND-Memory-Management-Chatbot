3d7e773b3b2e35ab3a99ab0ee2538db96cc840d0
alice <bow1226@gmail.com>
2020-03-14 16:30:12 +0800

    fix: Use emplace_back(std::move(edge)) and clean lvalue

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -27,14 +27,14 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 {
-    _childEdges.push_back(std::unique_ptr<GraphEdge>(edge));
+    _childEdges.emplace_back(std::move(edge));
 }
 
 //// STUDENT CODE
 ////
-void GraphNode::MoveChatbotHere(ChatBot &&chatbot)
+void GraphNode::MoveChatbotHere(ChatBot chatbot)
 {
     _chatBot = std::move(chatbot);
     _chatBot.GetChatLogicHandle()->SetChatbotHandle(
